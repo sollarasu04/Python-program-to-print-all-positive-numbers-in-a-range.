b990958a98ee1594b57b9fa3f5683f02541ad4cf
sollarasu04 <75630761+sollarasu04@users.noreply.github.com>
2020-12-27 23:47:37 +0530

    Delete listing.py

--- listing.py
+++
@@ -1,8 +0,0 @@
-#Write a Python program to print all positive numbers in a range.
-list1 = [12,-7,5,64,-14]
-list2 = [12,14,-95,3]
-list3 = list1 + list2
-print(list3) 
-for num in list3:
-    if num>0:
-        print(num, end=" ")
